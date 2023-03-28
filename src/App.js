3aecaad3ffde8af5e8ff86af38a454f61ee3275a
Leslie <Kooliesk@gmail.com>
2023-03-28 13:47:30 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import './App.css';
 
 import Home from "./Routes/Home";
 import About from "./Routes/About";
 import Login from "./Routes/Login";
 import Profile from "./Routes/Profile";
+import NotMatch from "./Routes/NotMatch";
+import Layout from "./components/Layout";
 
 const TodoApp = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} />
-      <Route path="profile" element={<Profile />} />
+      <Route  path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
     </Routes>
   );
 };
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,15 +1,12 @@
-import '../App.css';
 import Header from '../components/Header';
 import TodosLogic from '../components/TodosLogic';
 
 function Home() {
   return (
-    <div className="wrapper">
       <div className="todos">
         <Header />
         <TodosLogic />
       </div>
-    </div>
   );
 }
 
